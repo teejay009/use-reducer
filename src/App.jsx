c3cb04ef6998ec628381e2ced86e632ff3414ec8
teejay009 <tajdjimoh@gmail.com>
2023-11-09 14:28:21 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 const App = () => {
   const [useInput, setUseImput] = useState('')
   const [count, setCount] = useState(0)
-  const [color, setColor] = useState(false),
+  const [color, setColor] = useState(false)
   
 
   
   return (
-    <main className="App">
+    <main className="App" style={color ? '#fff92'}>
       <input
         
         onChange={handleInputChange}
