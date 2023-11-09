ff11b5b98807bea4966e83882c310a7a06f21dfa
teejay009 <tajdjimoh@gmail.com>
2023-11-09 14:26:39 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,16 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [randomInput, setRandomInput] = useState("");
-  const [Seconds, setSeconds] = useState(0);
+  const [useInput, setUseImput] = useState('')
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState(false)
+  
 
-  const renders = useRef(0);
-
-  const inputRef = useRef();
-
-  const timerId = useRef();
-
-  const startTimer = () => {
-    timerId.current = setInterval(() => {
-      renders.current++;
-      setSeconds((prev) => prev + 1);
-    }, 1000);
-    inputRef.current.focus();
-  };
-
-  const stopTimer = () => {
-    clearInterval(timerId.current);
-    timerId.current = 0;
-    inputRef.current.focus();
-  };
-
-  const resetTimer = () => {
-    stopTimer()
-    if(Seconds){
-      renders.current++
-      setSeconds(0)
-    }
-    inputRef.current.focus();
-  }
-
-  const handleInputChange = (e) => {
-    setRandomInput(e.target.value);
-
-    renders.current++;
-  };
-
-  //const focusOnInput = () =>{
-  //inputRef.current.focus()
-  // }
-
+  
   return (
     <main className="App">
       <input
-        ref={inputRef}
+        
         onChange={handleInputChange}
         type="text"
         value={randomInput}
