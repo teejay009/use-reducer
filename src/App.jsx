cc487fcefc8ff8ec8fd64167f3bdaeaf29adcebb
teejay009 <tajdjimoh@gmail.com>
2023-11-09 15:55:45 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -47,9 +47,9 @@ const App = () => {
 
 
       <section>
-        <button onClick={(() => dispatch({type: 'ACTION.DECREMENT'}))}>-</button>
-        <button onClick={(() => dispatch({type: 'ACTION.INCREMENT'}))}>+</button>
-       <button onClick={(() => dispatch({type: 'ACTION.TG_COLOR'}))}>Color</button>
+        <button onClick={(() => dispatch({type: ACTION.DECREMENT}))}>-</button>
+        <button onClick={(() => dispatch({type: ACTION.INCREMENT}))}>+</button>
+       <button onClick={(() => dispatch({type: ACTION.TG_COLOR}))}>Color</button>
       </section>
 
       
